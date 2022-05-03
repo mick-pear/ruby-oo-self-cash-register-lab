753c056374f86651b7aa0f3d0318283b19e8e08e
Matthew <matthew_mcaleer@hotmail.com>
2022-05-03 22:03:28 +1000

    commit tree 37302f8f8ca89553db7e99bcb7ae21e4ea9283c4

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,22 @@
+class CashRegister
+    attr_accessor :total, :employee_discount, :items
 
+    def initialize(employee_discount=0)
+        @total = 0
+        @employee_discount = employee_discount
+        @items = []
+    end
+
+    def add_item(title, price)
+        
+        items << title
+    end
+
+    def apply_discount
+        if employee_discount != 0
+            self.total * employee_discount
+        else
+            "There is no discount to apply."
+        end
+    end
+end
